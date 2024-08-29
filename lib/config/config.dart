d35535de5b4d9c21a05f425c27e113f1850644ba
developercolombia17 <developer.colombia17@airissoluciones.es>
2024-08-28 21:15:17 -0500

    build: flavors added

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -2,3 +2,4 @@ export './theme/app_theme.dart';
 export './constants/environment.dart';
 export './router/app_router.dart';
 export './notifications/local_notifications.dart';
+export './flavors.dart';
